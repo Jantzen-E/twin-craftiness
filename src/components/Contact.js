f2b7a1e6d93d8a6becc6671e57ac294302da6514
Jantzen-E <jantzen.egan@gmail.com>
2021-01-21 22:18:20 -0700

    added hover effects and updated email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
     return(
         <div className="contact">
             <h1>Contact us at 
-                <span className="email"><span> </span><a href="mailto:thecutecraftshack.happilyhomeade@gmail.com">thecutecraftshack.happilyhomeade@gmail.com</a><span> </span></span>
+                <span className="email"><span> </span><a href="mailto:thecutecraftshack@gmail.com">thecutecraftshack@gmail.com</a><span> </span></span>
                 to order something that you want and to tell us how you want it customized for you.
             </h1>
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import background from '../images/background.jpg';
  
 function Home() {
     return(
-        <div className="homePage" style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', width: '100%', height: "1200px", backgroundPosition: 'center', backgroundSize: 'cover' }}>
+        <div className="homePage" id="gradient"
+        style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', width: '100%', height: "1600px", backgroundPosition: 'center', backgroundSize: 'cover' }}>
             
         </div>
     )
