684b31fff430fa84e2410afc37ee074546d89aab
Jantzen-E <jantzen.egan@gmail.com>
2021-05-03 21:38:56 -0600

    update email address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
     return(
         <div className="contact">
             <h1>Contact us at 
-                <span><span> </span><a href="mailto:thecutecraftshack@gmail.com" className="email">thecutecraftshack@gmail.com</a><span> </span></span>
+                <span><span> </span><a href="mailto:thecutecraftshack.happilyhomade@gmail.com" className="email">thecutecraftshack.happilyhomade@gmail.com</a><span> </span></span>
                 to order something that you want and to tell us how you want it customized for you.
             </h1>
         </div>
